534a42e8a6ea989d0f33409fc26a4074ae459a2c
Dmitry <nerosketch@gmail.com>
2016-12-25 18:39:19 +0000

    вывел в начало самые важные классы

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,3 +1,10 @@
-from models import Abonent, Tariff
-from main import main
-from sslTransmitter import get_TransmitterClientKlass, NetExcept
+# -*- coding: utf8 -*-
+from mod_mikrotik import MikrotikTransmitter
+from core import NasFailedResult
+from structs import TariffStruct, AbonStruct
+
+
+# Transmitter мы будем импортировать в других местах
+# Тут надо указать какой у нас будет NAS
+# т.е. какой класс будет управлять доступом в интернет
+Transmitter = MikrotikTransmitter
