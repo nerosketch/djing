ea8c67ba02fa17c7626ae6cac2e5eb4f7fddbb8e
Dmitry <nerosketch@gmail.com>
2016-12-26 00:24:58 +0000

    Данные NAS по умолчанию

--- agent/settings.py
+++ agent/settings.py
@@ -9,3 +9,8 @@ KEYFILE = "/etc/ssl/server.key"
 
 # Использовать-ли при передаче инфы между NAS и основным сервером SSL
 IS_USE_SSL = False
+
+NAS_IP = '10.12.1.11'
+NAS_LOGIN = 'admin'
+NAS_PASSW = '2ekc3'
+NAS_PORT = 8728
