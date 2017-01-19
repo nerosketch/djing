966fa8cc372bdd644aaa30589298b0d18dddbdfa
Dmitry <nerosketch@gmail.com>
2017-01-19 22:38:27 +0000

    переключение порта

--- devapp/urls.py
+++ devapp/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^(?P<did>\d+)$', views.devview, name='view'),
     url(r'^(?P<did>\d+)/del$', views.devdel, name='del'),
     url(r'^(?P<devid>\d+)/edit$', views.dev, name='edit'),
+    url(r'^(?P<did>\d+)/(?P<portid>\d+)_(?P<status>[0-1]{1})$', views.toggle_port, name='port_toggle')
 ]
