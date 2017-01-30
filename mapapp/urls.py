a27e36acc6ea861ba630fbd0c7b8690b3580d828
Dmitry <nerosketch@gmail.com>
2017-01-30 12:17:58 +0000

    сделал точку

--- mapapp/urls.py
+++ mapapp/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     url(r'^options/(?P<did>\d+)/remove$', views.remove, name='remove_dot'),
     url(r'^get_dots$', views.get_dots, name='get_dots'),
 
-    url(r'^modal_add_dot$', views.modal_add_dot, name='modal_add_dot')
+    url(r'^modal_add_dot$', views.modal_add_dot, name='modal_add_dot'),
+    url(r'^j_dot_tooltip$', views.dot_tooltip, name='dot_tooltip')
 ]
