3dc6523fb64774e9969cd3728ba7bfead065d91b
Dmitry <nerosketch@gmail.com>
2017-02-04 20:10:53 +0000

    убрал не используемый context processor

--- djing/settings_example.py
+++ djing/settings_example.py
@@ -65,7 +65,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'global_context_processors.context_processor_client_ipaddress',
                 'taskapp.context_proc.get_active_tasks_count',
                 'global_context_processors.context_processor_additional_profile'
             ],
