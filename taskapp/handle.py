82a7281e1113f112b42bb150042d004b973bc980
Dmitry <nerosketch@gmail.com>
2017-02-21 02:15:50 +0300

    Реализовал перевод

--- taskapp/handle.py
+++ taskapp/handle.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.utils.translation import ugettext as _
 from chatbot.telebot import send_notify
 from chatbot.models import ChatException
 
@@ -10,7 +11,7 @@ class TaskException(Exception):
 def handle(task, author, recipient, abon_group):
     try:
         dst_account = recipient
-        text = 'Задача'
+        text = _('Task')
         # Если сигнал самому себе то молчим
         if author == recipient:
             return
@@ -19,17 +20,17 @@ def handle(task, author, recipient, abon_group):
             return
         # Если задача завершена
         elif task.state == 'F':
-            text = 'Задача завершена'
+            text = _('Task completed')
             # Меняем телефон назначения на телефон автора, т.к. при завершении
             # идёт оповещение автору о выполнении
             dst_account = author
         fulltext="%s: %s. " % (text, task.abon.get_full_name())
         if task.abon:
-            fulltext += "по адресу %s тел. %s. " % (
+            fulltext += _('address %s. telephone %s. ') % (
                 task.abon.address, task.abon.telephone
             )
-        fulltext += "с. %s. Тип задачи - %s. " % (abon_group.title, task.get_mode_display())
-        fulltext += task.descr if task.descr else '<без описания>'
+        fulltext += _('locality %s. Task type - %s. ') % (abon_group.title, task.get_mode_display())
+        fulltext += task.descr if task.descr else _('Not assigned')
         send_notify(fulltext, dst_account)
     except ChatException as e:
         raise TaskException(e)
