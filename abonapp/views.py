56f396d209b257e7c46ceb0627515c5cbb6ca75b
http <http@localhost.localdomain>
2017-03-29 16:03:30 +0300

    сортировка групп по умолчанию по алфавиту

--- abonapp/views.py
+++ abonapp/views.py
@@ -64,7 +64,7 @@ def addgroup(request):
 @login_required
 @mydefs.only_admins
 def grouplist(request):
-    groups = models.AbonGroup.objects.annotate(usercount=Count('abon'))
+    groups = models.AbonGroup.objects.annotate(usercount=Count('abon')).order_by('title')
 
     # фильтр
     directory, field = mydefs.order_helper(request)
