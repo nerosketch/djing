4e507d5743e6f25b8952c6c5ffed4b2cfc88f81a
http <http@localhost.localdomain>
2017-04-26 12:02:20 +0300

    coming soon...

--- agent/netflow/netflow_handler.sh
+++ agent/netflow/netflow_handler.sh
@@ -9,9 +9,16 @@ fi
 
 CUR_DIR=`dirname $0`
 
-DUMP_FILE="$CUR_DIR/$FNAME"
+DUMP_FILE="/tmp/djing_flow/$FNAME"
 PATH=/usr/local/sbin:/usr/local/bin:/usr/bin
+TMP_DUMP=/tmp/djing_flow/djing_flow_dump.tmp
+
+cd $CUR_DIR
+
+mv $DUMP_FILE $TMP_DUMP
 
 
-flow-print -f3 < ${DUMP_FILE} | ${CUR_DIR}/to_mysql \
-| mysql -uroot -p jungagent --password=ps
+./djing_flow < $TMP_DUMP | /usr/bin/mysql -uUSER -h <IP Database> -p <DBUSER> --password=<DB_PASSWORD>
+
+rm $TMP_DUMP
+
