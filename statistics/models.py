ae181c61b9f384bf13a6fcc7382fd206a9df651f
bashmak <nerosketch@gmail.com>
2017-05-31 17:44:08 +0300

    Добавил модель кеша статистики

--- statistics/models.py
+++ statistics/models.py
@@ -1,25 +1,13 @@
 import math
 from datetime import datetime, timedelta, date, time
-from django.db import models, ProgrammingError, connection
+from django.db import models, connection
+from django.utils.timezone import now
+
 from mydefs import MyGenericIPAddressField
 from .fields import UnixDateTimeField
-from mydefs import LogicError
 
 
 class StatManager(models.Manager):
-
-    def traffic_by_ip(self, ip):
-        try:
-            traf = self.order_by('-cur_time').filter(ip=ip, octets__gt=524288)[0]
-            now = datetime.now()
-            if traf.cur_time < now - timedelta(minutes=55):
-                return False, traf
-            else:
-                return True, traf
-        except IndexError:
-            return False, None
-        except ProgrammingError as e:
-            raise LogicError(e)
 
     def chart(self, ip_addr, count_of_parts=12, want_date=date.today()):
         def byte_to_mbit(x):
@@ -42,7 +30,7 @@ class StatManager(models.Manager):
             charts_times = split_list(charts_times, count_of_parts)
             charts_times = [avarage(t) for t in charts_times]
 
-            charts_data = map(lambda x, y: (x, y), charts_times, charts_octets)
+            charts_data = zip(charts_times, charts_octets)
             charts_data = ["{x: new Date(%d), y: %.2f}" % (cd[0], cd[1]) for cd in charts_data]
             midnight = datetime.combine(want_date, time.min)
             charts_data.append("{x:new Date(%d),y:0}" % (int(charts_times[-1:][0]) + 1))
@@ -97,10 +85,27 @@ class StatElem(models.Model):
         abstract = True
 
 
-def getModel(want_date=datetime.now()):
+def getModel(want_date=now()):
 
     class DynamicStatElem(StatElem):
         class Meta:
             db_table = 'flowstat_%s' % want_date.strftime("%d%m%Y")
             abstract = False
     return DynamicStatElem
+
+
+class StatCache(models.Model):
+    last_time = UnixDateTimeField()
+    ip = MyGenericIPAddressField(primary_key=True)
+    octets = models.PositiveIntegerField(default=0)
+    packets = models.PositiveIntegerField(default=0)
+
+    def is_online(self):
+        return self.last_time < now() - timedelta(minutes=55)
+
+    def is_today(self):
+        return date.today() == self.last_time.date()
+
+
+    class Meta:
+        db_table = 'flowcache'
