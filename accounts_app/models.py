3ce2b2b25a713bd2e1d085815505f1538fa98cba
Dmitry Novikov <nerosketch@gmail.com>
2018-01-19 11:53:17 +0300

    fix bugs

--- accounts_app/models.py
+++ accounts_app/models.py
@@ -3,7 +3,7 @@ import os
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django.core.validators import RegexValidator
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from photo_app.models import Photo
 
