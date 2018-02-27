1d86d4819bddae1d10870c106a547cc738112134
Дмитрий <leming.1993@mail.ru>
2018-02-28 00:55:00 +0600

    Update install_debian.sh

--- install_debian.sh
+++ install_debian.sh
@@ -1,3 +1,3 @@
 /usr/bin/bash -
 
-apt-get install mariadb-server mariadb-client libmysqlclient-dev python3-dev python3-pip python3-pillow uwsgi nginx uwsgi-plugin-python3 libsnmp-dev git 
+apt-get install mariadb-server mariadb-client default-libmysqlclient-dev python3-dev python3-pip python3-pil uwsgi nginx uwsgi-plugin-python3 libsnmp-dev git 
