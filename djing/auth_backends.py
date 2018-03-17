bcdd33ea0fadafe7b09f2c748b231ecf5386e8f9
bashmak <nerosketch@gmail.com>
2018-03-18 00:26:35 +0300

    Fix some bugs

---
+++ djing/auth_backends.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.backends import ModelBackend
+from .models import BaseAccount, UserProfile
+
+
+
+class CustomAuthBackend(ModelBackend):
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
+        try:
+            user = UserModel._default_manager.get_by_natural_key(username)
+        except UserModel.DoesNotExist:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a non-existing user (#20760).
+            UserModel().set_password(password)
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
