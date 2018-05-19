9ff1180bf2a4338f280d84a7916876d73bdc5876
bashmak <nerosketch@gmail.com>
2018-05-19 12:24:53 +0300

    Add remove button for group

--- group_app/urls.py
+++ group_app/urls.py
@@ -6,5 +6,6 @@ app_name = 'group_app'
 urlpatterns = [
     url(r'^$', views.GroupListView.as_view(), name='group_list'),
     url(r'^add/$', views.AddGroupView.as_view(), name='add'),
-    url(r'^(?P<group_id>\d{1,6})/edit/$', views.EditGroupView.as_view(), name='edit')
+    url(r'^(?P<group_id>\d{1,6})/edit/$', views.EditGroupView.as_view(), name='edit'),
+    url(r'^(?P<group_id>\d{1,6})/del/$', views.DeleteGroupView.as_view(), name='del')
 ]
--- group_app/views.py
+++ group_app/views.py
@@ -1,10 +1,13 @@
-from django.views.generic import ListView, UpdateView, CreateView
+from django.db.models import Count
+from django.http import HttpResponseRedirect
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import gettext_lazy as _
 from django.utils.decorators import method_decorator
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.conf import settings
+from guardian.decorators import permission_required_or_403 as permission_required
 from . import models
 from . import forms
 
@@ -50,3 +53,22 @@ class AddGroupView(CreateView):
     def form_invalid(self, form):
         messages.success(self.request, _('Please fix form errors'))
         return super(AddGroupView, self).form_invalid(form)
+
+
+@method_decorator(login_required, name='dispatch')
+@method_decorator(permission_required('group_app:delete_group'), name='dispatch')
+class DeleteGroupView(DeleteView):
+    model = models.Group
+    pk_url_kwarg = 'group_id'
+    success_url = reverse_lazy('group_app:group_list')
+
+    def delete(self, request, *args, **kwargs):
+        group_id = self.kwargs.get('group_id')
+        group_with_subscribers = models.Group.objects.annotate(
+            subscribers_count=Count('abon')
+        ).filter(subscribers_count__gt=0, pk=group_id).first()
+        print('group_with_subscribers:', group_with_subscribers)
+        if group_with_subscribers is not None:
+            messages.error(request, _('Group is contain subscribers. Remove them before delete group'))
+            return HttpResponseRedirect(self.success_url)
+        return super().delete(request, *args, **kwargs)
