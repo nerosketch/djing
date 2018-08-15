9633c6e9080b811b176d782f4b45ece9e26e0805
Dmitry Novikov <nerosketch@gmail.com>
2018-08-15 14:24:45 +0300

    split sql

--- statistics/migrations/0003_auto_20180814_1921.py
+++ statistics/migrations/0003_auto_20180814_1921.py
@@ -14,13 +14,15 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL(
-            "DROP TABLE `flowcache`; "
-            "CREATE TABLE `flowcache` ( "
-            "  `last_time` INT(10) UNSIGNED NOT NULL, "
-            "  `abon_id`   INT(11) DEFAULT NULL UNIQUE, "
-            "  `octets`    INT(10) UNSIGNED NOT NULL, "
-            "  `packets`   INT(10) UNSIGNED NOT NULL, "
-            "  KEY `flowcache_abon_id_91e1085d` (`abon_id`) "
-            ") ENGINE = MEMORY  DEFAULT CHARSET = utf8;"
+            (
+                "DROP TABLE `flowcache`;",
+                "CREATE TABLE `flowcache` ( "
+                "  `last_time` INT(10) UNSIGNED NOT NULL, "
+                "  `abon_id`   INT(11) DEFAULT NULL UNIQUE, "
+                "  `octets`    INT(10) UNSIGNED NOT NULL, "
+                "  `packets`   INT(10) UNSIGNED NOT NULL, "
+                "  KEY `flowcache_abon_id_91e1085d` (`abon_id`) "
+                ") ENGINE = MEMORY  DEFAULT CHARSET = utf8;"
+            )
         )
     ]
