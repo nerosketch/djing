9468d3bfaa7814636f0a28d2f1d49fc1cc49d1db
Dmitry Novikov <nerosketch@gmail.com>
2018-08-28 10:37:41 +0300

    make temporary directory if not exist

--- agent/netflow/start_netflow.sh
+++ agent/netflow/start_netflow.sh
@@ -2,5 +2,6 @@
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/bin
 
-flow-capture -R /var/www/djing/agent/netflow/netflow_handler.py -p /run/flow.pid -w /tmp/djing_flow -n1 -N0 0/0/6343
+mkdir -p /tmp/djing_flow
 
+flow-capture -R /var/www/djing/agent/netflow/netflow_handler.py -p /run/flow.pid -w /tmp/djing_flow -n1 -N0 0/0/6343
