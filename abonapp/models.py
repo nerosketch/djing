94cdf1fd64479a1f00a54f499fa0044772ce3efe
Dmitry Novikov <nerosketch@gmail.com>
2018-11-16 15:25:52 +0300

    Add tasks for abonapp

--- abonapp/models.py
+++ abonapp/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import Optional
 
+from abonapp.tasks import user_remove_from_gw, user_add_to_gw, user_nas_sync
 from accounts_app.models import UserProfile, MyUserManager, BaseAccount
 from bitfield import BitField
 from django.conf import settings
@@ -317,16 +318,7 @@ class Abon(BaseAccount):
         """
         if self.nas is None:
             raise LogicError(_('gateway required'))
-        try:
-            agent_abon = self.build_agent_struct()
-            if agent_abon is not None:
-                mngr = self.nas.get_nas_manager()
-                mngr.update_user(agent_abon)
-        except (NasFailedResult, NasNetworkError, ConnectionResetError) as e:
-            print('ERROR:', e)
-            return e
-        except LogicError:
-            pass
+        user_nas_sync.delay(self.pk)
 
     def nas_add_self(self):
         """
@@ -335,16 +327,7 @@ class Abon(BaseAccount):
         """
         if self.nas is None:
             raise LogicError(_('gateway required'))
-        try:
-            agent_abon = self.build_agent_struct()
-            if agent_abon is not None:
-                mngr = self.nas.get_nas_manager()
-                mngr.add_user(agent_abon)
-        except (NasFailedResult, NasNetworkError, ConnectionResetError) as e:
-            print('ERROR:', e)
-            return e
-        except LogicError:
-            pass
+        user_add_to_gw.delay(self.pk)
 
     def nas_remove_self(self):
         """
@@ -353,16 +336,7 @@ class Abon(BaseAccount):
         """
         if self.nas is None:
             raise LogicError(_('gateway required'))
-        try:
-            agent_abon = self.build_agent_struct()
-            if agent_abon is not None:
-                mngr = self.nas.get_nas_manager()
-                mngr.remove_user(agent_abon)
-        except (NasFailedResult, NasNetworkError, ConnectionResetError) as e:
-            print('ERROR:', e)
-            return e
-        except LogicError:
-            pass
+        user_remove_from_gw.delay(self.pk)
 
     def get_absolute_url(self):
         return resolve_url('abonapp:abon_home', self.group.id, self.username)
