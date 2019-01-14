1c46eb39edb327536081d4ac129c80ba50702f6d
Dmitry Novikov <nerosketch@gmail.com>
2019-01-14 14:19:02 +0300

    refactoring

--- gw_app/nas_managers/mod_mikrotik.py
+++ gw_app/nas_managers/mod_mikrotik.py
@@ -301,7 +301,7 @@ class MikrotikTransmitter(core.BaseTransmitter, ApiRos,
         if queue_gw is None:
             return self.add_queue(queue)
         else:
-            cmd = [
+            cmd = (
                 '/queue/simple/set',
                 '=name=%s' % queue.name,
                 '=max-limit=%.3fM/%.3fM' % queue.max_limit,
@@ -311,10 +311,9 @@ class MikrotikTransmitter(core.BaseTransmitter, ApiRos,
                 '=queue=Djing_pcq_up/Djing_pcq_down',
                 '=burst-time=5/5',
                 '=burst-limit=%.3fM/%.3fM' % tuple(i * 2 for i in queue.max_limit),
-                '=burst-threshold=%.3fM/%.3fM' % tuple(i / 1.2 for i in queue.max_limit)
-            ]
-            if queue.queue_id:
-                cmd.insert(1, '=numbers=%s' % queue.queue_id)
+                '=burst-threshold=%.3fM/%.3fM' % tuple(i / 1.2 for i in queue.max_limit),
+                '=numbers=%s' % queue_gw.queue_id
+            )
             r = self._exec_cmd(cmd)
             return r
 
