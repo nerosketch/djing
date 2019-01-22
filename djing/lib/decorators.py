1f2f2bfa784d51b29687f37577a93686ac925d71
Dmitry Novikov <nerosketch@gmail.com>
2019-01-22 15:03:55 +0300

    fix

--- djing/lib/decorators.py
+++ djing/lib/decorators.py
@@ -99,9 +99,9 @@ def json_view(fn):
     @wraps(fn)
     def wrapped(request, *args, **kwargs):
         r = fn(request, *args, **kwargs)
-        if 'text' in r.keys() and not isinstance(r['text'], str):
+        if r and 'text' in r.keys() and not isinstance(r['text'], str):
             r['text'] = str(r['text'])
-        return JsonResponse(dict(r), safe=False, json_dumps_params={
+        return JsonResponse(dict(r or {'text': 'null'}), safe=False, json_dumps_params={
             'ensure_ascii': False
         })
     return wrapped
