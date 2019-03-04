83f7179c3dbca62dba5c68b3cdf4e2a070323a61
Dmitry Novikov <nerosketch@gmail.com>
2019-03-05 01:23:04 +0300

    revert to mysql

--- systemd_units/do_backup.sh
+++ systemd_units/do_backup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-PATH=/usr/bin:/usr/sbin:/bin
+PATH=/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
 
 cd /var/backups
 
 file="djing`date "+%Y-%m-%d_%H.%M.%S"`.sql.gz"
 
-export PGPASSWORD=POSTGRES ROOT PASSWORD
+mysql_passw=MYSQL ROOT PASSWORD
 
-pg_dump -O -d djing -h localhost -U djing | gzip > $file
+echo show tables | mysql -uroot -p$mysql_passw djingdb | \
+	grep -v '^flowstat' | grep -v 'traflost' | grep -v '^Tables' | \
+	xargs mysqldump -R -Q --add-locks -uroot --password=$mysql_passw djingdb $1 | gzip > $file
 
 chmod 400 $file
 ./webdav_backup.py $file
 
 # удаляем старые
 find . -name "djing20??-??-??_??.??.??.sql.gz" -mtime +30 -type f -delete
-
