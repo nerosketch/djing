04ab23ac447f09d419da9f1579120c9cac1490bc
Dmitry Novikov <nerosketch@gmail.com>
2019-04-15 15:14:45 +0300

    fix

--- agent/netflow/start_netflow.sh
+++ agent/netflow/start_netflow.sh
@@ -8,7 +8,7 @@ if ! [ -n "$1" ]; then
 fi
 
 port=$1
-
+DIRECTORY=`dirname $(readlink -e "$0")`
 mkdir -p /tmp/djing_flow
 
-flow-capture -R /home/ns/Projects/djing/agent/netflow/netflow_handler.sh -p /run/flow.pid -w /tmp/djing_flow -n1 -N0 0/0/${port}
+flow-capture -R ${DIRECTORY}/netflow_handler.sh -p /run/flow.pid -w /tmp/djing_flow -n1 -N0 0/0/${port}
