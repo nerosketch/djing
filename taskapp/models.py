9f295388382e089c47f59c3984aee39b71adc128
Dmitry Novikov <nerosketch@gmail.com>
2019-07-13 00:43:22 +0300

    ref

--- taskapp/models.py
+++ taskapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.conf import settings
 from django.shortcuts import resolve_url
 from django.utils import timezone
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext_lazy as _
 from abonapp.models import Abon
 from .handle import handle as task_handle
 
